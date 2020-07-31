a1d3ff2a4837a4ae93cb54596297ec008022ae84
ksoon1985 <ksoon1985@naver.com>
2020-07-31 10:41:30 +0900

    리소스 경로

--- 2team/src/main/java/kr/co/sol/custom/web/MemberController.java
+++ 2team/src/main/java/kr/co/sol/custom/web/MemberController.java
@@ -1,7 +1,12 @@
 package kr.co.sol.custom.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.co.sol.custom.service.MemberService;
 
@@ -10,4 +15,5 @@ public class MemberController {
 
 	@Autowired
 	MemberService memberService;
+	
 }
