2e52d3269d59e01491ba17ed2729ee1552f2b82c
clownstudy <angelos0424@gmail.com>
2020-08-07 12:09:46 +0900

    최근 커밋에 빠진 js 파일 추가

--- 2team/src/main/webapp/resources/js/adminIndex.js
+++ 2team/src/main/webapp/resources/js/adminIndex.js
@@ -1,7 +1,7 @@
 //검색창에 글자 쓰면 DB에서 매칭되는 항목 보여주기
 $( document ).ready(function() {
 	$("#searchBar").keyup(function(e) {
-		// 37~40 - 방향키 / 8 = backSpace / 45~46 = ins, del
+		// 37~40 - 방향키 / 8 = backSpace / 45~46 = ins, del 근데 생각해보니 필요 없잖아? 모든 상황에서 반응해도 딱히..
 		if(!(e.keyCode >= 37 && e.keyCode <= 40 || e.keyCode==8 || e.keyCode==45 || e.keyCode==46)){
 		var inputVal = $(this).val();
 		$(this).val(inputVal.replace(/[^(ㄱ-힣a-zA-Z0-9]/gi,'')); // 한글 숫자 영문만 가능
