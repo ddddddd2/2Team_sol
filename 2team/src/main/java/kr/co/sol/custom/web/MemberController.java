089129635c06d9b7eb9f258adc8c69481ecb51f7
clownstudy <angelos0424@gmail.com>
2020-08-20 21:19:53 +0900

    login처리 (ajax) loginPro로 구현(기존 loginProc 주석처리)

--- 2team/src/main/java/kr/co/sol/custom/web/MemberController.java
+++ 2team/src/main/java/kr/co/sol/custom/web/MemberController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,57 +27,66 @@ public class MemberController {
 	@RequestMapping(value="/")
 	public String index(HttpServletRequest request, Model model, HttpServletResponse response) {
 		
-		/* 굳이 할필요 없을듯... 
-		 * HttpSession session = request.getSession(); String idKey =
-		 * (String)session.getAttribute("idKey"); session.setAttribute("idKey", idKey);
-		 */
-		 
 		return "/custom/index";
 	} 
 	
 	// login page
 	@RequestMapping(value="/custom/login")
-	public String login() {
-
+	public String login(HttpServletRequest res) {
+		
 		return "/custom/login";
 	}
 	
-	// login process
-	@RequestMapping(value="/custom/loginProc")
-	public String loginProc(HttpServletRequest request, Model model, HttpServletResponse response,
-			MemberDTO mdto) {
-		
-		mdto = memberService.loginProc(mdto); 
-	    HttpSession session = request.getSession();
-		String mem_id= null;
-		String url="/";
-		
-		// 일치하는 아이디와 비밀번호가 있는경우 
-		if(mdto != null)
-		{
-			mem_id = mdto.getId(); // member의 id가져오고 
-			if(mdto.getRole().equals("admin")) // 역활이 admin 이라면 
-			{
-				session.setAttribute("adminDTO", mdto); // admin 세션을 추가
-			}else {
-				session.setAttribute("userDTO", mdto); // user 세션을 추가 
-			}
+	@PostMapping(value="/loginPro")
+	public @ResponseBody int loginPro(HttpServletRequest request, MemberDTO mdto, HttpSession session) {
+		mdto = memberService.loginPro(mdto);
+		if(mdto==null) {
+			return 0; // 아이디 비밀번호 조회가 실패
 		}
-		
-		// 로그인에 처리하지 못하였을 때 
-		if(mem_id == null) {
-			model.addAttribute("msg","id 또는 passwd 가 일치하지 않습니다. ");
-			url = "/custom/login";
+		if("admin".equals(mdto.getRole())) {
+			return 1; // 로그인한 아이디가 어드민 계정일 경우
 		}
-		else {
-			model.addAttribute("msg","로그인에 성공하였습니다. ");
-			session.setAttribute("idKey", mem_id);
-		}
-		
-		model.addAttribute("url",url);
-			
-		return "/custom/msgPage";
+		request.getSession();
+		session.setAttribute("idKey", mdto.getNo());
+		return 2;
 	}
+	
+//	// login process
+//	@RequestMapping(value="/custom/loginProc")
+//	public String loginProc(HttpServletRequest request, Model model, HttpServletResponse response,
+//			MemberDTO mdto) {
+//		
+//		mdto = memberService.loginProc(mdto); 
+//	    HttpSession session = request.getSession();
+//		String mem_id= null;
+//		String url="/";
+//		
+//		// 일치하는 아이디와 비밀번호가 있는경우 
+//		if(mdto != null)
+//		{
+//			mem_id = mdto.getId(); // member의 id가져오고 
+//			if(mdto.getRole().equals("admin")) // 역활이 admin 이라면 
+//			{
+//				session.setAttribute("adminDTO", mdto); // admin 세션을 추가
+//			}else {
+//				session.setAttribute("userDTO", mdto); // user 세션을 추가 
+//			}
+//		}
+//		
+//		// 로그인에 처리하지 못하였을 때 
+//		if(mem_id == null) {
+//			model.addAttribute("msg","id 또는 passwd 가 일치하지 않습니다. ");
+//			url = "/custom/login";
+//		}
+//		else {
+//			model.addAttribute("msg","로그인에 성공하였습니다. ");
+//			session.setAttribute("idKey", mem_id);
+//		}
+//		
+//		model.addAttribute("url",url);
+//			
+//		return "/custom/msgPage";
+//	}
 	
 	// logout process
 	@RequestMapping(value="/custom/logout")
