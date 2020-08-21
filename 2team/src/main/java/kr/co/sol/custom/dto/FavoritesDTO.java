e138a7e89a1d7873eea54e5642089370b7fd66bc
clownstudy <angelos0424@gmail.com>
2020-08-21 21:52:51 +0900

    없는 DTO 추가

---
+++ 2team/src/main/java/kr/co/sol/custom/dto/FavoritesDTO.java
@@ -0,0 +1,13 @@
+package kr.co.sol.custom.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class FavoritesDTO {
+	private int res_no;
+	private int mem_no;
+}
