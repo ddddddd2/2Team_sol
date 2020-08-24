6f39832e43526108ce619d76dc2c7e6281a13798
ksoon1985 <ksoon1985@naver.com>
2020-08-24 11:47:40 +0900

    @Controller 추가 ...

--- 2team/src/main/java/kr/co/sol/custom/web/SearchResultController.java
+++ 2team/src/main/java/kr/co/sol/custom/web/SearchResultController.java
@@ -3,12 +3,14 @@ package kr.co.sol.custom.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.sol.custom.dto.RestaurantDTO;
 import kr.co.sol.custom.searchresult.service.SearchResultService;
 
+@Controller 
 public class SearchResultController {
 	
 	@Autowired
