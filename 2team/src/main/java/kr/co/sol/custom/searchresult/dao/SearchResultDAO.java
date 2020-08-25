7c53a09333deb8517a8471287764e42b2e86aa30
ksoon1985 <ksoon1985@naver.com>
2020-08-25 20:17:43 +0900

    sub1 음식점 리스트와 지도 구현 중 ... sh:순환

--- 2team/src/main/java/kr/co/sol/custom/searchresult/dao/SearchResultDAO.java
+++ 2team/src/main/java/kr/co/sol/custom/searchresult/dao/SearchResultDAO.java
@@ -3,11 +3,12 @@ package kr.co.sol.custom.searchresult.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import kr.co.sol.custom.dto.RestaurantDTO;
 
 @Mapper
 public interface SearchResultDAO {
 	List<RestaurantDTO> getRestaurants(RestaurantDTO resdto);
-
+	List<RestaurantDTO> getRestaurants2(@Param("keyword") String keyword);
 }
